4a668edcd8ca2b3e26d2c098b05f4e13f906fbf7
Astro <88717385+astrogames06@users.noreply.github.com>
2024-01-01 11:21:53 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gistId = 'c049b8379985c4531c9c6d799249a21b';
-const accessToken = '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'; // Use a personal access token with the "gist" scope
+const accessToken = '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'; // Use a personal access token with the "gist" scope
 
 // JavaScript code to manage the Gist JSON list
 let charactersArray = [];
